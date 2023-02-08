8aa1509cb9d740f9d5c01998fed9959c517f8bcf
+ <jkunins95@gmail.com>
2023-02-08 14:19:03 -0500

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(a) {
+    return a + 1
+}
+
+function decrement(a) {
+    return a - 1
+}
+
+function makeInt(a) {
+    let number = parseInt(a, 10);
+    return number;
+}
+
+function preserveDecimal(a) {
+    let number = parseFloat(a, 10);
+    return number;
+}
